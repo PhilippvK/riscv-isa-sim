4953ec8eb67b6be65d15bf30cf7da39dde0443ec
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-07-05 16:38:32 +0200

    Revert "DRY in store-conditional instructions"
    This reverts commit 7b52a249c8259e1968e6edeec3fbd673c0e55dd5.

--- riscv/insns/sc_w.h
+++ riscv/insns/sc_w.h
@@ -1,5 +1,10 @@
 require_extension('A');
 
-bool have_reservation = MMU.store_conditional<uint32_t>(RS1, RS2);
+bool have_reservation = MMU.check_load_reservation(RS1, 4);
+
+if (have_reservation)
+  MMU.store_uint32(RS1, RS2);
+
+MMU.yield_load_reservation();
 
 WRITE_RD(!have_reservation);
